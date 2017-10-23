c0ec5c0da0e16248defbad18056fae26af14c0c2
Gr4ph0s <gr4ph0s@hotmail.fr>
2017-10-23 13:08:58 +0200

    Fix BasicUsage example

--- Exemple/BasicUsage.py
+++ Exemple/BasicUsage.py
@@ -1,44 +1,44 @@
 import c4d
 from RedshiftWrapper.Redshift import Redshift
- 
+
 def main():
-    rs = RedshiftWrapper()
-   
+    rs = Redshift()
+
     #Assign Material
     rs.SetMat(doc.GetFirstMaterial())
-   
+
     #Create Constant Node
     ConstNode = rs.CreateShader(c4d.ID_OPERATOR_CONST, x=100, y=500)
-   
+
     #Create Fresnel shader and expose parameter
     FresnelNode = rs.CreateShader("Fresnel", x=200, y=500)
     FresnelNode.ExposeParameter(c4d.REDSHIFT_SHADER_FRESNEL_USER_CURVE, c4d.GV_PORT_INPUT)
-   
+
     #Connect Constant and fresnel
-    rs.CreateConnection(ConstNode, 0, FresnelNode, 0)
-   
+    rs.CreateConnection(ConstNode, FresnelNode, 0, 0)
+
     #Get default material node and output node
     listNode = rs.GetAllNodes()
     MatNode = None
     OutPutNode = None
     for node in listNode:
-        if node.GetNodeType() == "Output":
+        if node.GetType() == "Output":
             OutPutNode = node
-        elif node.GetNodeType() == "Material":
+        elif node.GetType() == "Material":
             MatNode = node
-           
+
     if not MatNode or not OutPutNode:
         return
-   
+
     #Expose a paramter of the material
     MatNode.ExposeParameter(c4d.REDSHIFT_SHADER_MATERIAL_DIFFUSE_WEIGHT, c4d.GV_PORT_INPUT)
-   
+
     #Connect Fresnel shader to the material
-    rs.CreateConnection(FresnelNode, 0, MatNode, 0)
-   
+    rs.CreateConnection(FresnelNode, MatNode, 0, 0)
+
     #Connect material shader to the output
-    rs.CreateConnection(MatNode, 0, OutPutNode, 0)
-   
+    rs.CreateConnection(MatNode, OutPutNode, 0, 0)
+
     c4d.EventAdd()
 if __name__=='__main__':
     main()
